105c173658468946b5523716c45eeb05365d9ca8
Sivar-Developer <fragmatica.dc@gmail.com>
2022-05-29 12:06:50 +0300

    Instance to dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 import json
 from django.shortcuts import render
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
 from products.models import Product
@@ -9,7 +10,6 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        # Convert model instance to json
+        data = model_to_dict(model_data, fields=['id','title'])
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,6 +5,7 @@ import requests
 endpoint = "http://localhost:8000/api"
 
 get_response = requests.get(endpoint)
+# print(get_response.headers)
 # print(get_response.text)
 print(get_response.json())
 # print(get_response.status_code)
