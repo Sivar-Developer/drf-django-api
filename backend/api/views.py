5c5b508239e1781f986232244047b1f9f12fbf66
Sivar-Developer <fragmatica.dc@gmail.com>
2022-05-29 11:14:36 +0300

    Get data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,23 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
-    return JsonResponse({ "message": "Django api response" })
+    print(request.GET)
+    print(request.POST)
+    body = request.body
+    data = {}
+    
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    print(data)
+    
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,9 +2,9 @@ import requests
 
 # endpoint = "http://httpbin.org/status/200"
 # endpoint = "http://httpbin.org/anything"
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api"
 
 get_response = requests.get(endpoint)
 print(get_response.text)
-print(get_response.jsno())
+print(get_response.json())
 print(get_response.status_code)
