06efd760f698840483b220d3d8998d5b270c64f6
Sivar-Developer <fragmatica.dc@gmail.com>
2022-05-29 15:29:35 +0300

    Rest framework view and response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,16 @@
-import json
-from django.shortcuts import render
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
 # Create your views here.
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         # Convert model instance to json
         data = model_to_dict(model_data, fields=['id','title'])
-    return JsonResponse(data)
+    return Response(data)
