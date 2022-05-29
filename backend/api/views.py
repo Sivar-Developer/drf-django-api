6895c2e1643e8e79cd88e9a32fe287bcd8930ab6
Sivar-Developer <fragmatica.dc@gmail.com>
2022-05-29 11:37:56 +0300

    Django model instance as API response

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,22 +2,14 @@ import json
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from products.models import Product
+
 # Create your views here.
 def api_home(request, *args, **kwargs):
-    print(request.GET)
-    print(request.POST)
-    body = request.body
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    
-    print(data)
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-    print(data)
-    
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,6 +5,6 @@ import requests
 endpoint = "http://localhost:8000/api"
 
 get_response = requests.get(endpoint)
-print(get_response.text)
+# print(get_response.text)
 print(get_response.json())
-print(get_response.status_code)
+# print(get_response.status_code)
